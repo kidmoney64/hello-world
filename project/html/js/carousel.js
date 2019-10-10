89a95edec9adbcd2ed465da29c5598f44ce20a2d
kidmoney64 <kidmoney64@users.noreply.github.com>
2019-10-09 20:45:05 -0500

    add some parts of the carousel still adding the jss

---
+++ project/html/js/carousel.js
@@ -0,0 +1,4 @@
+const track = document.querySelector(carousel__track);
+const track = Array.from(track.children);
+const nextButton = document.querySelector(' .carousel__button--right')
+const prevButton = document.querySelector(' .carousel__button--left')
